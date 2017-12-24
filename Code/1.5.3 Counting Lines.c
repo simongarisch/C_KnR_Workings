ab1f13b0af7701bcdc3402e21fa95fb0c12dabf8
simongarisch <gatman946@gmail.com>
2017-12-25 06:43:50 +1100

    Adding counting examples

--- Code/1.5.3 Counting Lines.c
+++ Code/1.5.3 Counting Lines.c
@@ -7,6 +7,8 @@ int main(){
   int c, nl;
   nl = 0;
   while((c = getchar()) != EOF){
+    // A character  written between single quotes represents an integer value equal
+    // to the numerical value of the character in the machine's character set
     if(c == '\n'){
       ++nl;
     }
