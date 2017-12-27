bc11fe8d33310a6c29aa084a8ea40f36d9073d38
simongarisch <gatman946@gmail.com>
2017-12-28 07:39:47 +1100

    Added character array example

---
+++ Code/1.9 Character Arrays.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#define MAXLINE 1000 // maximum input line size
+
+int getline2(char line[], int maxline);
+void copy(char to[], char from[]);
+
+// print longest input line
+int main(){
+  int len;               // current line length 
+  int max;               // maximum length seen so far
+  char line[MAXLINE];    // current input line
+  char longest[MAXLINE]; // longest line saved here
+
+  max = 0;
+  while((len = getline2(line, MAXLINE)) > 0){
+    if(len > max){
+      max = len;
+      copy(longest, line); 
+    }
+  }
+
+  if(max > 0){
+    printf("%s", longest);
+  } 
+
+  return(0);
+}
+
+
+// getline2: read a line into s, return length
+int getline2(char s[], int lim){
+  int c, i;
+  for(i = 0; (i<lim-1 && (c=getchar())!=EOF && c!='\n'); ++i){
+    s[i] = c;
+  }
+  if(c == '\n'){
+    s[i] = c;
+    ++i;
+  }
+  s[i] = '\0';
+  return(i);
+}
+
+
+// copy: copy 'from' into 'to' and assume 'to' is big enough
+void copy(char to[], char from[]){
+  int i=0;
+  while((to[i] = from[i]) != '\0'){
+    ++i;
+  }
+}
